77c87db31c3050074716ca17159e7e2277145d87
julia kharlamova <juliettkhar@gmail.com>
2020-08-09 07:34:24 +0300

    Updates

--- js/choose.ca79f806.js
+++ js/choose.ca79f806.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["choose"],{"1c91":function(e,t,n){"use strict";n.r(t);var c=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("router-view")},a=[],o=n("9ab4"),r=n("60a3"),u=function(e){function t(){return null!==e&&e.apply(this,arguments)||this}return Object(o["b"])(t,e),t=Object(o["a"])([Object(r["a"])({name:"Choose"})],t),t}(r["b"]),s=u,i=s,b=n("2877"),l=Object(b["a"])(i,c,a,!1,null,"f703abe2",null);t["default"]=l.exports}}]);
-//# sourceMappingURL=choose.edc3b5ca.js.map
+//# sourceMappingURL=choose.ca79f806.js.map
